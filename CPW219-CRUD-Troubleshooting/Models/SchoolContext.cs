1895147dc81f1ff059d12e7a3157d4cdc7b0197c
Jessie <jessietajalle08@gmail.com>
2024-08-21 16:16:27 -0700

    Created database

--- CPW219-CRUD-Troubleshooting/Models/SchoolContext.cs
+++ CPW219-CRUD-Troubleshooting/Models/SchoolContext.cs
@@ -6,7 +6,7 @@ namespace CPW219_CRUD_Troubleshooting.Models
     {
         public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
         {
-
+            
         }
 
         public DbSet<Student> Students { get; set; }
